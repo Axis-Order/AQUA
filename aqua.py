9b95d25b03ce070f42386b916aa3760482443589
hirokazu <axis-order@protonmail.com>
2020-05-14 07:23:52 +0900

    calc is success2

--- aqua.py
+++ aqua.py
@@ -200,6 +200,7 @@ def repl():
                 stack.push(com)
                 STATUS = 0
                 continue
+                #aaa
             if isNumeric(com):
                 b = float(com)
                 op = stack.pop()
